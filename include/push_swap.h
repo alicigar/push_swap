628cb8c6a99135dc50f80d8599a10c25050cd202
alicigar <aliciamomoko@gmail.com>
2025-09-09 20:25:59 +0200

    in progress

--- include/push_swap.h
+++ include/push_swap.h
@@ -15,16 +15,18 @@
 
 # include "../libft/libft.h"
 
-typedef struct s_node
+/*typedef struct s_node
 {
 	int				value;
 	int				index;
 	struct s_node	*next;
-}					t_node;
+}					t_node;*/
 
-int	is_valid_digit(char *str);
-int	check_dupes(int *values, int args_num);
-int	between_int(const char *nptr);
-int	parsing_validation(char	**args, int args_num, int *values);
+int		is_valid_digit(char *str);
+int		check_dupes(int *numbers, int count);
+int		between_int(const char *nptr);
+int		empty_str(char *str);
+int		parsing_validation(char	**args, int args_num, int *values);
+void	free_ft_split(char **array_of_numbers);
 
 #endif
