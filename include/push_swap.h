af3de8c31f650271811d05c8e8b4319613f6be48
alicigar <aliciamomoko@gmail.com>
2025-09-19 00:14:23 +0200

    definitive

--- include/push_swap.h
+++ include/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: alicigar < alicigar@student.42malaga.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/01 13:09:38 by alicigar          #+#    #+#             */
-/*   Updated: 2025/09/05 16:35:10 by alicigar         ###   ########.fr       */
+/*   Updated: 2025/09/18 23:45:28 by alicigar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,32 @@
 
 # include "../libft/libft.h"
 
-/*typedef struct s_node
-{
-	int				value;
-	int				index;
-	struct s_node	*next;
-}					t_node;*/
-
 int		is_valid_digit(char *str);
 int		check_dupes(int *numbers, int count);
 int		between_int(const char *nptr);
 int		empty_str(char *str);
 int		parsing_validation(char	**args, int args_num, int *values);
 void	free_ft_split(char **array_of_numbers);
+char	**prepare_args(int argc, char **argv);
+char	**init_args(int argc, char **argv, int *count);
+int		count_argc(int argc, char **args);
+void	sa(int *stack, int count);
+void	ra(int *stack, int count);
+void	rra(int	*stack, int count);
+void	pa(int *stack_a, int *size_a, int *stack_b, int *size_b);
+void	pb(int *stack_a, int *size_a, int *stack_b, int *size_b);
+void	sort_two(int *stack);
+void	sort_three(int *stack);
+void	sort_five(int *stack_a, int *size_a, int *stack_b, int *size_b);
+void	sort_radix(int *a, int count, int size_b, int max_bits);
+int		get_max_bits(int count, int max_bits);
+void	sorting(int count, int *indexed);
+void	index_numbers(int *numbers, int *indexed, int count);
+int		is_sorted(int *array, int count);
+int		check_errors(int argc, char **argv, char **args);
+int		parse_check(int *numbers, char **args, int count, int argc);
+int		index_check(int *indexed, int argc, char **args, int *numbers);
+int		check_is_sorted(int *indexed, int count, int argc, char **args);
+int		count_args(char **args);
 
 #endif
